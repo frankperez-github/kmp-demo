48e6b13c6487f311b7aada347b3b804716b349b0
frankperez-github <fp848584@gmail.com>
2025-11-10 12:46:53 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import KMPVisualizer from './components/kmp.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <KMPVisualizer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
